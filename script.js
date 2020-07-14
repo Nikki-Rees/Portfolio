36898143785bd1301e52c725af71b4e4169001f8
Nikki-Rees <nikki-rees@outlook.com>
2020-07-14 20:41:53 +1000

    Updates to styling and resume added

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.getElementById("pdfResume").onclick = function () {
+//     location.href = ""
+// };
+
+document.getElementById("viewMVP").onclick = function () {
+    location.href = " https://nikki-rees.github.io/MyMVP/"
+};
+
+
+document.getElementById("viewWeatherDashboard").onclick = function () {
+    location.href = "https://nikki-rees.github.io/weather_dashboard/;"
+};
+
+
+document.getElementById("viewPlanner").onclick = function () {
+    location.href = "https://nikki-rees.github.io/planner/"
+};
