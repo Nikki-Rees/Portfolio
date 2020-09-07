7c1a5a4abab01c92743eef504448da5d364c3590
Nikki-Rees <nikki-rees@outlook.com>
2020-09-07 19:39:24 +1000

    update to links and js on click

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ document.getElementById("viewMVP").onclick = function () {
 
 
 document.getElementById("viewWeatherDashboard").onclick = function () {
-    location.href = "https://nikki-rees.github.io/weather_dashboard/;"
+    location.href = "https://nikki-rees.github.io/weather_dashboard/"
 };
 
 
 document.getElementById("viewPlanner").onclick = function () {
     location.href = "https://nikki-rees.github.io/planner/"
+};
+
+document.getElementById("viewTinySteps").onclick = function () {
+    location.href = "https://tinystepsau.herokuapp.com/"
+};
+
+document.getElementById("viewNoteTaker").onclick = function () {
+    location.href = "https://secret-basin-76195.herokuapp.com/notes"
+};
+
+document.getElementById("viewEmployeeManager").onclick = function () {
+    location.href = "https://drive.google.com/file/d/1hUU4k8isVT59pv1Qlb1dumb1UipCM95b/view"
 };
